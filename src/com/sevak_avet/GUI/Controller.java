bd073a4ad868091a33dd9c44a6e550da1a91ee95
Avetisyan Sevak <Sevak_Avet@mail.ru>
2014-02-05 23:44:53 +0400

    Reformated Controller class

--- src/com/sevak_avet/GUI/Controller.java
+++ src/com/sevak_avet/GUI/Controller.java
@@ -87,100 +87,41 @@ public class Controller implements Initializable {
 	
 	private static String schedule = "";
 	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("Controller initialized!..");
-		
-		months = new TextField[]{monthNum1, monthNum2, monthNum3, monthNum4, monthDenom1, monthDenom2, monthDenom3, monthDenom4};
-		periodsNum = new TextField[]{periodsNum1, periodsNum2, periodsNum3, periodsNum4};
-		periodsDenom = new TextField[]{periodsDenom1, periodsDenom2, periodsDenom3, periodsDenom4};
-		
-		EventHandler<ActionEvent> actionURL = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {				
-				if(!periodsIsEmpty() && !tfURL.getText().isEmpty()) {
-					try {
-						DOMParser dp = DOMParser.getInstance();
-						URL url = new URL(tfURL.getText() + "/lesson");
-						String path = new File(".").getAbsolutePath() + "schedule.xls";
-						String[][] table = dp.parse(url, path);
-						
-						String[] curSession = isWinterSession ? winterSession : summerSession;
-						ScheduleMarkup markup = new ScheduleMarkup(table, curSession, getPeriodData(periodsNum), getPeriodData(periodsDenom));
-						schedule = markup.getSchedule();
-						
-						btnSave.setDisable(false);
-					} catch (SAXException e) {
-						e.printStackTrace();
-					} catch (ParserConfigurationException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					} catch (OperationNotSupportedException e) {
-						e.printStackTrace();
-					}
-				} else {
-					System.out.println("Some periods or URL is empty!..");
+	private final EventHandler<ActionEvent> actionURL = new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent event) {				
+			if(!periodsIsEmpty() && !tfURL.getText().isEmpty()) {
+				try {
+					DOMParser dp = DOMParser.getInstance();
+					URL url = new URL(tfURL.getText() + "/lesson");
+					String path = new File(".").getAbsolutePath() + "schedule.xls";
+					String[][] table = dp.parse(url, path);
+					
+					String[] curSession = isWinterSession ? winterSession : summerSession;
+					ScheduleMarkup markup = new ScheduleMarkup(table, curSession, getPeriodData(periodsNum), getPeriodData(periodsDenom));
+					schedule = markup.getSchedule();
+					
+					btnSave.setDisable(false);
+				} catch (SAXException e) {
+					e.printStackTrace();
+				} catch (ParserConfigurationException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (OperationNotSupportedException e) {
+					e.printStackTrace();
 				}
+			} else {
+				System.out.println("Some periods or URL is empty!..");
 			}
-		};
-		
-		EventHandler<ActionEvent> actionOpenFile = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {				
-				if(!periodsIsEmpty()) {
-					FileChooser chooser = new FileChooser();
-					chooser.getExtensionFilters().add(new ExtensionFilter("Расписание", Arrays.asList("*.xml", "*.xls")));
-					
-					File desktop = new File(System.getProperty("user.home") + "\\Рабочий стол");
-					if(!desktop.exists()) {
-						desktop = new File(System.getProperty("user.home") + "\\Desktop");
-					}
-					
-					if(desktop.exists()) {
-						chooser.setInitialDirectory(desktop);
-					}
-					
-					File file = chooser.showSaveDialog(new Stage(StageStyle.UTILITY));
-					
-					if(file != null) {
-						System.out.println(file);
-						
-						try {
-							DOMParser dp = DOMParser.getInstance();
-							String[][] table = dp.parse(file);
-							
-							String[] curSession = isWinterSession ? winterSession : summerSession;
-							ScheduleMarkup markup = new ScheduleMarkup(table, curSession, getPeriodData(periodsNum), getPeriodData(periodsDenom));							
-							schedule = markup.getSchedule();
-							
-							btnSave.setDisable(false);
-						} catch (SAXException e) {
-							e.printStackTrace();
-						} catch (ParserConfigurationException e) {
-							e.printStackTrace();
-						} catch (IOException e) {
-							e.printStackTrace();
-						} catch (OperationNotSupportedException e) {
-							e.printStackTrace();
-						}
-						
-						btnSave.setDisable(false);	
-					} else {
-						System.out.println("File not choosed!");
-					}
-					
-				} else {
-					System.out.println("Some periods is empty!..");
-				}
-			}
-		};
-		
-		EventHandler<ActionEvent> actionSaveToFile = new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
+		}
+	};
+	private final EventHandler<ActionEvent> actionOpenFile = new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent event) {				
+			if(!periodsIsEmpty()) {
 				FileChooser chooser = new FileChooser();
-				chooser.getExtensionFilters().add(new ExtensionFilter("Текстовый файл", Arrays.asList("*.txt")));
+				chooser.getExtensionFilters().add(new ExtensionFilter("Расписание", Arrays.asList("*.xml", "*.xls")));
 				
 				File desktop = new File(System.getProperty("user.home") + "\\Рабочий стол");
 				if(!desktop.exists()) {
@@ -194,19 +135,104 @@ public class Controller implements Initializable {
 				File file = chooser.showSaveDialog(new Stage(StageStyle.UTILITY));
 				
 				if(file != null) {
-					System.out.println(file.getAbsolutePath());
-					try(PrintWriter pw = new PrintWriter(file)) {
-						System.out.println("WRITING");
-						System.out.println(schedule);
-						pw.write(schedule);
-					} catch (FileNotFoundException e) {
+					System.out.println(file);
+					
+					try {
+						DOMParser dp = DOMParser.getInstance();
+						String[][] table = dp.parse(file);
+						
+						String[] curSession = isWinterSession ? winterSession : summerSession;
+						ScheduleMarkup markup = new ScheduleMarkup(table, curSession, getPeriodData(periodsNum), getPeriodData(periodsDenom));							
+						schedule = markup.getSchedule();
+						
+						btnSave.setDisable(false);
+					} catch (SAXException e) {
+						e.printStackTrace();
+					} catch (ParserConfigurationException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (OperationNotSupportedException e) {
 						e.printStackTrace();
 					}
+					
+					btnSave.setDisable(false);	
 				} else {
-					System.out.println("File save path not choosed!");
+					System.out.println("File not choosed!");
 				}
+				
+			} else {
+				System.out.println("Some periods is empty!..");
 			}
-		};
+		}
+	};
+	private final EventHandler<ActionEvent> actionSaveToFile = new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent event) {
+			FileChooser chooser = new FileChooser();
+			chooser.getExtensionFilters().add(new ExtensionFilter("Текстовый файл", Arrays.asList("*.txt")));
+			
+			File desktop = new File(System.getProperty("user.home") + "\\Рабочий стол");
+			if(!desktop.exists()) {
+				desktop = new File(System.getProperty("user.home") + "\\Desktop");
+			}
+			
+			if(desktop.exists()) {
+				chooser.setInitialDirectory(desktop);
+			}
+			
+			File file = chooser.showSaveDialog(new Stage(StageStyle.UTILITY));
+			
+			if(file != null) {
+				System.out.println(file.getAbsolutePath());
+				try(PrintWriter pw = new PrintWriter(file)) {
+					System.out.println("WRITING");
+					System.out.println(schedule);
+					pw.write(schedule);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("File save path not choosed!");
+			}
+		}
+	};
+	private final ChangeListener<String> choiceBoxTypeListener = new ChangeListener<String>() {
+		@Override
+		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			if(newValue.equals("URL")) {
+				btnLoad.setText("Загрузить");
+				setTFEnabled(true);
+				
+				btnLoad.setOnAction(actionURL);
+			} else {
+				btnLoad.setText("Открыть");
+				setTFEnabled(false);
+				
+				btnLoad.setOnAction(actionOpenFile);
+			}
+		}
+	};
+	private final ChangeListener<String> choiceBoxSessionListener = new ChangeListener<String>() {
+		@Override
+		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			if(newValue.equals("Зимняя")) {
+				chooseSession(winterSession);
+				isWinterSession = true;
+			} else {
+				chooseSession(summerSession);
+				isWinterSession = false;
+			}
+		}
+	};
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println("Controller initialized!..");
+		
+		months = new TextField[]{monthNum1, monthNum2, monthNum3, monthNum4, monthDenom1, monthDenom2, monthDenom3, monthDenom4};
+		periodsNum = new TextField[]{periodsNum1, periodsNum2, periodsNum3, periodsNum4};
+		periodsDenom = new TextField[]{periodsDenom1, periodsDenom2, periodsDenom3, periodsDenom4};
 		
 		btnLoad.setOnAction(actionURL);
 		btnSave.setOnAction(actionSaveToFile);
@@ -214,38 +240,12 @@ public class Controller implements Initializable {
 		
 		choiceBoxType.getItems().addAll("URL", "Файл");
 		choiceBoxType.getSelectionModel().select(0);
-		choiceBoxType.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if(newValue.equals("URL")) {
-					btnLoad.setText("Загрузить");
-					setEnabled(tfURL, true);
-					
-					btnLoad.setOnAction(actionURL);
-				} else {
-					btnLoad.setText("Открыть");
-					setEnabled(tfURL, false);
-					
-					btnLoad.setOnAction(actionOpenFile);
-				}
-			}
-		});
+		choiceBoxType.getSelectionModel().selectedItemProperty().addListener(choiceBoxTypeListener);
 		
 		choiceBoxSession.getItems().addAll("Летняя", "Зимняя");
 		choiceBoxSession.getSelectionModel().select(0);
 		chooseSession(summerSession);
-		choiceBoxSession.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if(newValue.equals("Зимняя")) {
-					chooseSession(winterSession);
-					isWinterSession = true;
-				} else {
-					chooseSession(summerSession);
-					isWinterSession = false;
-				}
-			}
-		});		
+		choiceBoxSession.getSelectionModel().selectedItemProperty().addListener(choiceBoxSessionListener);		
 	}
 	
 	private String[] getPeriodData(TextField[] array) {
@@ -259,13 +259,13 @@ public class Controller implements Initializable {
 		return data;
 	}
 	
-	private void setEnabled(TextField tf, boolean enabled) {
+	private void setTFEnabled(boolean enabled) {
 		if(enabled) {
-			tf.setEditable(true);
-			tf.setOpacity(1.0);
+			tfURL.setEditable(true);
+			tfURL.setOpacity(1.0);
 		} else {
-			tf.setEditable(false);
-			tf.setOpacity(0.5);
+			tfURL.setEditable(false);
+			tfURL.setOpacity(0.5);
 		}
 	}
 	
